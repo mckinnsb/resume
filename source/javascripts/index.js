373262c8d17067060c09300add9e8e3b0313753f
Stew McKinney <mckinnsb@gmail.com>
2016-08-29 18:44:20 -0400

    an attempt at some email obf

---
+++ source/javascripts/index.js
@@ -0,0 +1,27 @@
+var RESUME = RESUME || {};
+
+( function () {
+
+  RESUME.mailMe = function() {
+
+    var coded = "YnhExxdL@qYyEv.nUY",
+        key = "HAtLcDvPMUwhKdi0rjp7BInZmJGg5fVWEa2QN3T8xuY96q4bSFlyzoXkRse1CO",
+        shift=coded.length,
+        link="";
+
+    for (i=0; i<coded.length; i++) {
+      if (key.indexOf(coded.charAt(i))==-1) {
+        ltr = coded.charAt(i)
+        link += (ltr)
+      }
+      else {
+        ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+        link += (key.charAt(ltr))
+      }
+    }
+
+    window.location.href = "mailto:"+link;
+
+  };
+
+} () );
