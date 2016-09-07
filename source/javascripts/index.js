4bb070652b59b13dc37e1c4ac343006786cbfdc8
Stew McKinney <mckinnsb@gmail.com>
2016-09-07 19:56:36 -0400

    simple js to mimic hover effect on mobile

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -2,7 +2,58 @@ var RESUME = RESUME || {};
 
 ( function () {
 
-  RESUME.mailMe = function() {
+  RESUME.mailMe = youveGotMail;
+
+  var shown;
+
+  function checkVisible( e ) {
+
+    var buffer,
+        elements = window.document.getElementsByClassName( "job_history" ),
+        element,
+        event,
+        height = window.clientHeight || window.innerHeight,
+        rect;
+
+    buffer = height / 4;
+
+    for( var i = 0; i < elements.length; i++ ) {
+
+      element = elements[ i ];
+      rect = element.getBoundingClientRect();
+
+      if( rect.top > 0 && ( rect.top + buffer ) < height ) {
+        break;
+      }
+      else {
+        element = null;
+      }
+
+    }
+
+    if( shown != null && shown != element ) {
+      shown.classList.remove( "hovered" );
+    }
+
+    if( element != null ) {
+      shown = element;
+      shown.classList.add( "hovered" );
+    }
+
+  }
+
+  var eventAttached = false;
+  function touchInit () {
+
+    if( !eventAttached ) {
+      console.log( "attaching event listener" );
+      window.addEventListener( "scroll", checkVisible );
+      eventAttached = true;
+    }
+
+  }
+
+  function youveGotMail () {
 
     var coded = "YnhExxdL@qYyEv.nUY",
         key = "HAtLcDvPMUwhKdi0rjp7BInZmJGg5fVWEa2QN3T8xuY96q4bSFlyzoXkRse1CO",
@@ -22,9 +73,13 @@ var RESUME = RESUME || {};
 
     window.location.href = "mailto:"+link;
 
-  };
+  }
 
-  window.document.addEventListener( "touchstart", function() {} );
+
+
+  window.document.addEventListener( "touchstart", touchInit );
+
+  window.onload = checkVisible;
 
 } () );
 
