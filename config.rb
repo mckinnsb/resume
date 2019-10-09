a3fad84863d154dfe59799fad432a9de0b4edfbe
Stew McKinney <mckinnsb@gmail.com>
2019-10-09 15:00:37 -0400

    adds pdf generation to build process and links it to resume

--- config.rb
+++ config.rb
@@ -24,9 +24,25 @@ activate :relative_assets
   proxy "#{name}.html", 'devlog.html', locals: { game: name }, ignore: true
 end
 
+# Simple class to save the resume as a PDF after build
 class PDFBuild < Middleman::Extension
-  def initialize(app, options = {}, &block); end
+  def initialize(app, options = {}, &block)
+    super
+  end
+
+  def after_build(_)
+    file = File.new 'build/resume.html'
+
+    kit = PDFKit.new(file,
+                     page_size: 'Letter',
+                     print_media_type: true)
+
+    kit.to_file('build/resume.pdf')
+  end
 end
+
+::Middleman::Extensions.register :pdf_build, PDFBuild
+activate :pdf_build
 
 ###
 # Helpers
