e14fbcd0eb95e59e09e4f15338f28d7c5409052c
Stew McKinney <mckinnsb@gmail.com>
2019-12-11 16:57:01 -0500

    ok, no bizarre yarn warnings this time

---
+++ secret_machine/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Stage } from '@inlet/react-pixi';
+
+function App() {
+  return (
+    <Stage/>
+  );
+}
+
+export default App;
