57301fb60c6fe9e24bc6bf1656328a6c4e3d1e17
Stew McKinney <mckinnsb@gmail.com>
2020-01-10 17:10:59 -0500

    updates build

---
+++ build/precache-manifest.3cec976fbf65bbbcd79aff6d79b7f256.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811dac1509123d531949265ec73fde19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c9a5b7945cb1c52f89b",
+    "url": "/static/css/main.c12d2646.chunk.css"
+  },
+  {
+    "revision": "2d84cbd850b11d1f7ecc",
+    "url": "/static/js/2.bc88daad.chunk.js"
+  },
+  {
+    "revision": "0c9a5b7945cb1c52f89b",
+    "url": "/static/js/main.55027f7a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "73745505f308285f59da2a0b87b9c519",
+    "url": "/static/media/background.73745505.jpg"
+  },
+  {
+    "revision": "252e74d87b394f2470c444067a27a99b",
+    "url": "/static/media/commodore.252e74d8.woff"
+  },
+  {
+    "revision": "d4c74a2aae001db8804fd52bf7d454b6",
+    "url": "/static/media/desklamp.d4c74a2a.svg"
+  },
+  {
+    "revision": "a4573174e15ee38fd3ea8fba349bd1d1",
+    "url": "/static/media/monitor.a4573174.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.714783f37db815adf121dbf26eb5149b.js"
+  "/precache-manifest.3cec976fbf65bbbcd79aff6d79b7f256.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
